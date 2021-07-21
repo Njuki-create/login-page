89bf8d7e9829f476022eb41b0b54be017a369521
Njuki-create <68369427+Njuki-create@users.noreply.github.com>
2021-07-21 04:33:21 +0300

    Add files via upload
    pushed the login-page project to the login-page GitHub repository

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    function myFunction(a) {
+        if (a == 1)
+            document.getElementById("forgotpassword").style.display = "none";
+        else
+            document.getElementById("forgotpassword").style.display = "block";
+    }
+}
